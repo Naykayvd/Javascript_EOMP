e08fb837f7ce5dcbcaddbc005f6a59c84098e556
NayKayvd <vandiemennahum@gmail.com>
2021-08-13 14:18:55 +0200

    commit updated

--- show-products.js
+++ show-products.js
@@ -1,5 +1,3 @@
-// let container = document.querySelector(".item-box1")
-
 fetch("http://127.0.0.1:5000/products-page/")
 .then((request) => {
     // console.log(request.json())
@@ -18,23 +16,6 @@ fetch("http://127.0.0.1:5000/products-page/")
                 <p>price-R: ${item[2]}</p>
                 <button class="add-btn">add to cart <i class="fas fa-cart-plus"></i></button>
                 </div>`;
-            // if (item_no == 3){
-            //     box++;
-            //     item_no = 0;
-            //     console.log(box);
-            //     console.log(item_no);
-            //     createBox(box);
-            // } else {
-            //     console.log(item_no);
-            //     let box_container = document.querySelector(`.item-box${box}`);
-            //     box_container.innerHTML += `<div class="item">
-            //     <p>item:${item_no}</p>
-            //     <img class="item-img" src="images/f8a458e18503c86603dc02339a944feb_XL.jpg" alt="">
-            //     <p>price:100</p>
-            //     <button class="add-btn">add to cart <i class="fas fa-cart-plus"></i></button>
-            //     </div>`;
-            //     item_no++;
-            // }
         });
     })
 })
